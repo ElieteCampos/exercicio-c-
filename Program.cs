576d5dd5d0b2f08d70bca90ee3261dd25c2ada62
Eliete Campos <92051709+ElieteCampos@users.noreply.github.com>
2025-06-23 18:48:44 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using ExercicioFixacaoConstrutores;
+using System;
+using System.Globalization;
+
+namespace ExFixacaoConstrutores {
+    class Constutores {
+        static void Main(string[] args) {
+            ContaBank conta;
+
+            Console.Write( "Entre o número da conta: " );
+            int numero = int.Parse( Console.ReadLine() );
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Hávera depósito inicial (s / n)? ");
+            char resposta = char.Parse( Console.ReadLine() );
+            if (resposta == 's' || resposta == 'S') {
+                Console.Write("Entre o valor do deposito inicial: ");
+                double depositoInicial = double.Parse( Console.ReadLine(), CultureInfo.InvariantCulture );
+                conta = new ContaBank(numero, titular, depositoInicial);
+            }
+            else { 
+            conta = new ContaBank(numero, titular);
+            }
+            Console.WriteLine();
+                Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para deposito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:  ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados:  ");
+            Console.WriteLine(conta);
+
+        }
+
+
+
+
+    }
+}
